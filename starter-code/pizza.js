3e11e78a2aa910e9a0480ccd8ec4d5c25ad8a2f2
John Fish <j31@users.noreply.github.com>
2018-05-31 17:29:53 +0200

    made dynamic

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,73 +1,113 @@
-
+// wait for DOM to load
 $(document).ready(function() {
   
-  // Initialize defaults
-  var price = 13;
-  $(".crust").removeClass("crust-gluten-free")
-  $(".sauce").removeClass("sauce-white")
-  $(".btn-crust").removeClass("active")
-  $(".btn-sauce").removeClass("active")
-  $("#whi").addClass("hide")
-  $("#glu").addClass("hide")
-  updatePrice(0)
+  var ingredients = [
+    { topping: 'green peppers',   price: 1,  selected: true },
+    { topping: 'mushrooms',       price: 1,  selected: true  },
+    { topping: 'pepperonni',      price: 1,  selected: true },
+    { topping: 'sauce',           price: 3,  selected: true },
+    { topping: 'gluten-free crust',price: 5, selected: false },
+    { topping: 'pinapple',        price: 1,  selected: false },
+    { topping: 'hot peppers',     price: 2,  selected: false },
+    { topping: 'sausage',         price: 3,  selected: false },
+    { topping: 'pastrami',         price: 3,  selected: false },
+    { topping: 'cornichons',         price: 3,  selected: false },
+  ];
   
-  $(".btn-green-peppers").click(function(){
-    $(".green-pepper").toggle();
-    $(".btn-green-peppers").toggleClass('active')
-    $("#gre").toggleClass("hide")
-    if($(this).hasClass('active'))
-      updatePrice(1)
-    else
-      updatePrice(-1)
-  })
   
-  $(".btn-mushrooms").click(function(){
-    $(".cap").toggle();
-    $(".stem").toggle();
-    $(".btn-mushrooms").toggleClass('active')
-    $("#mus").toggleClass("hide")
-    if($(this).hasClass('active'))
-      updatePrice(1)
-    else
-      updatePrice(-1)
-  })
+  // ingredients constructor
+  function Pizza () {
+    // load toppings from ingredients array
+    this.toppings = ingredients;
+    this.basePrice = 10;
+  }
   
-  $(".btn-pepperonni").click(function(){
-    $(".pep").toggle();
-    $(".btn-pepperonni").toggleClass('active')
-    $("#pep").toggleClass("hide")
-    if($(this).hasClass('active'))
-      updatePrice(1)
-    else
-     updatePrice(-1)
-  })
+  Pizza.prototype.makeButtons = function() {
+   
+    for (var i=0; i<pizza.toppings.length; i++)
+    var html = '';
+    pizza.toppings.forEach(function (item, index) {
+      
+      var act = '';
+      if ( item.selected )
+      act = 'active'
+      
+      var id = 'b-' + index;
+      html += '<li><button id="' + id + 
+      '" class="btn btn-crust ' + act +
+      '">' + item.topping + 
+      '</button></li>';
+    });  
+    return html;  
+  }
   
-  $(".btn-sauce").click(function(){
-    $('.sauce').toggleClass('sauce-white')
-    $(".btn-sauce").toggleClass('active')
-    $("#whi").toggleClass("hide")
-    if($(this).hasClass('active'))
-      updatePrice(3)
-    else
-      updatePrice(-3)
-  })
-  
-  $(".btn-crust").click(function(){
-    $('.crust').toggleClass('crust-gluten-free')
-    $(".btn-crust").toggleClass('active')
-    $("#glu").toggleClass("hide")
-    if($(this).hasClass('active'))
-      updatePrice(5)
-    else
-      updatePrice(-5)
-  })
+  Pizza.prototype.listenButtons = function() {
+    console.log("make buttons method")
+    pizza.toppings.forEach(function (item, index) {
+      var sel = '#b-' + index;
+     
+      $(sel).click(function(){
+        // $(".pep").toggle();
+       
+        $(sel).toggleClass('active')
+        
+        if (item.selected) 
+          item.selected = false;
+        else
+          item.selected = true;
+        // $("#pep").toggleClass("hide") 
 
-  function updatePrice(n) {
-    price += n
-    var pricestr = "$" + price;
-    $("#price").text(pricestr)
+        var html = pizza.displayPrice();
+        $('#prices').html(html);
+
+        var total = pizza.calculatePrice();
+        var html = '$' + total;
+        $('#total').html(html);
+      });  
+    });  
+  }
+  
+
+  Pizza.prototype.calculatePrice = function() {
+    
+    return pizza.toppings.reduce(function (total, topping) {
+      
+      if (topping.selected)
+        return total + topping.price;
+      else
+        return total
+    },pizza.basePrice) 
   }
 
+  
+  Pizza.prototype.displayPrice = function() {
+    
+    for (var i=0; i<pizza.toppings.length; i++)
+    var html = '';
+    pizza.toppings.forEach(function (item, index) {
+      if (item.selected) {
+        html += '<li>$' + item.price + ' ' + item.topping + '</li>';
+      }
+    });    
+    return html;
+  }
+  
+  // instanciate class
+  var pizza = new Pizza();
+  
+  // Initialize page
+  var html = pizza.makeButtons();
+  $('#buttons').html(html);
+  
+  var html = pizza.displayPrice();
+  $('#prices').html(html);
+
+  var total = pizza.calculatePrice();
+  var html = '$' + total;
+  $('#total').html(html);
+  
+  pizza.listenButtons();
+  
 });
 
 
